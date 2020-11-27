cac3d8e80536cf20b9b706f5f8aa3a12dcd5d528
OguraShota <p1_ku6@icloud.com>
2020-11-27 14:36:58 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  with_options presence: true do
+    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
+    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
+    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
+    validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
+    validates :encrypted_password, format: { with: /\A[a-z0-9]+\z/i }
+    validates :birth_day
+  end
+    
+    
 end
