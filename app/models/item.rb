612e81eca3d542cd8346a1200a5822b54d813069
OguraShota <p1_ku6@icloud.com>
2020-11-28 16:17:48 +0900

    ActiveStoragenの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,17 @@
 class Item < ApplicationRecord
 
   belongs_to :user
+  has_one_attached :image
+
+  with_options presence: true do
+    validates :name
+    validates :category_id
+    validates :condition_id
+    validates :description
+    validates :postage_type_id
+    validates :preparation_day_id
+    validates :ship_from_id
+    validates :price
+    validates :user
+  end
 end
