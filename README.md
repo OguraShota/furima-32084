84b568790724346e538f9d392848cd5bb6d2c0bc
OguraShota <p1_ku6@icloud.com>
2020-12-04 17:52:20 +0900

    テーブルへの保存処理

--- README.md
+++ README.md
@@ -42,7 +42,7 @@
 | city                 | string     | null: false |
 | house_number         | string     | null: false |
 | building_name        | string     |
-| phone_number         | integer    | null: false |
+| phone_number         | string    | null: false |
 | purchase_record      | references | null: false, foreign_key: true |
 
 - belongs_to : purchase_record
--- app/controllers/purchase_records_controller.rb
+++ app/controllers/purchase_records_controller.rb
@@ -1,16 +1,24 @@
 class PurchaseRecordsController < ApplicationController
   def index
     @order_address = OrderAddress.new
+    @item = Item.find(params[:item_id])
   end
 
   def create
-
+    @item = Item.find(params[:item_id])
+    @order_address = OrderAddress.new(purchase_record_params)
+    if @order_address.valid?
+      @order_address.save
+      redirect_to root_path
+    else
+      render action: :index
+    end
   end
 
   private
 
   def purchase_record_params
-    params.require(:order_address).permit(:postal_code, :prefectures_id, :city, :house_number, :building_name, :phone_number )
+    params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 
 end
