6dc799fc4ff0a2389bafde91597b157d795edf7b
OguraShota <p1_ku6@icloud.com>
2020-12-05 14:35:32 +0900

    Payjpの導入

--- app/controllers/purchase_records_controller.rb
+++ app/controllers/purchase_records_controller.rb
@@ -8,6 +8,7 @@ class PurchaseRecordsController < ApplicationController
     @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new(purchase_record_params)
     if @order_address.valid?
+      pay_item
       @order_address.save
       redirect_to root_path
     else
@@ -17,8 +18,17 @@ class PurchaseRecordsController < ApplicationController
 
   private
 
+  def pay_item
+    Payjp.api_key = "sk_test_a98cb2b1ea6ff4d284839055"
+    Payjp::Charge.create(
+      amount: @item[:price],
+      card: purchase_record_params[:token],
+      currency: 'jpy'
+    )
+  end
+
   def purchase_record_params
-    params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
 
 end
--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,9 +1,9 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number, :purchase_record_id, :item_id, :user_id
+  attr_accessor :postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number, :purchase_record_id, :item_id, :user_id, :token
 
   with_options presence: true do
-    
+    validates :token
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/ } 
     validates :city, format: { with: /\A[ぁ-んァ-ン一-龥]/}
     validates :house_number
