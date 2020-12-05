e867ccfde0adce0d7374cd9b890f32b4b2395a9c
OguraShota <p1_ku6@icloud.com>
2020-12-05 22:16:08 +0900

    編集、削除ボタン表示の条件式

--- app/controllers/purchase_records_controller.rb
+++ app/controllers/purchase_records_controller.rb
@@ -1,16 +1,15 @@
 class PurchaseRecordsController < ApplicationController
   before_action :authenticate_user!, only: [:index]
+  before_action :set_item, only: [:index, :create]
 
   def index
-    @order_address = OrderAddress.new
-    @item = Item.find(params[:item_id])
+    @order_address = OrderAddress.new 
     if current_user == @item.user || @item.purchase_record.present?
       redirect_to root_path
     end 
   end
 
   def create
-    @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new(purchase_record_params)
     if @order_address.valid?
       pay_item
@@ -34,6 +33,10 @@ class PurchaseRecordsController < ApplicationController
 
   def purchase_record_params
     params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
   end
 
 end
