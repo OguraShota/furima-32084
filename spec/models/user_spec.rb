ca1bea9734e2334c304c2ebf97b946059e24f349
OguraShota <p1_ku6@icloud.com>
2020-12-07 15:26:12 +0900

    ユーザー登録修正(password)

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -47,6 +47,13 @@ RSpec.describe User, type: :model do
       expect(@user).to be_valid
     end
 
+    it 'passwordが5文字以下だと登録できない' do
+      @user.password = 'yama0'
+      @user.password_confirmation = 'yama0'
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Password is too short (minimum is 6 characters)", "Password is invalid", "Password confirmation is invalid")
+    end
+
     it 'passwordは、確認用を含めて2回入力すること' do
       @user.password = 'yamada090'
       @user.password_confirmation = nil
